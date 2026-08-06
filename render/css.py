0a51a18c25d4269f11cd36a4c6cddfc43f0eb930
Евгений Минко <evgenijminko@evgenmac.local>
2026-08-06 14:26:38 +0300

    new

--- render/css.py
+++ render/css.py
@@ -797,17 +797,18 @@ STRAT = """
 .g-lead{flex:0 0 auto;display:flex;flex-direction:column;
   justify-content:space-between;align-items:flex-end;height:196px;padding:8px 0; position: absolute;
     top: 0;transform: translateY(-43%);right: 10%}
+.lead-list::-webkit-scrollbar-thumb {
+    border-radius: 5px;
+    border: 1px solid #ffa5002b;
+    box-shadow: inset 0 0 5px #000;
+    padding-right: 6px;
+}
 .lead-list{display:flex;flex-direction:column;align-items:flex-end;gap:6px;margin-bottom: 20px; overflow: auto}
 .lead-list::-webkit-scrollbar {
   width: 1px; /* ширина полосы */
 }
 .lead-list::-webkit-scrollbar-track {
   background: transparent; /* цвет дорожки */
-}
-.lead-list::-webkit-scrollbar-thumb {
-    border-radius: 5px;
-    border: 1px solid #ffa5002b;
-    box-shadow: inset 0 0 5px #000;
 }
 .lead-t{font-size: 7px;font-weight: 300;letter-spacing: 2.5px;color: rgb(232 234 238 / 56%)}
 .lead-t.lead-g{color: #d4b476;}
